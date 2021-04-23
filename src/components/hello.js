3028108ba44f2cb773e03c45849f95cda708f57c
Luciana001 <luciana.brancato@gmail.com>
2021-04-23 12:24:42 +0200

    structure react

---
+++ src/components/hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello = () => {
+   return (
+      <div>
+         <h2>Pomodoro Clock</h2>
+         <p>{"Hello, World !"}</p>;
+      </div>
+   )
+  
+}
+
+export default Hello
